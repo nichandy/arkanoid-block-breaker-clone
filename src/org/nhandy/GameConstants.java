ef0414f54d08029892cbd1663c3590c54a4bef6a
Nick Handy <nichandy@gmail.com>
2020-05-16 15:57:34 -0700

    Added Arkanoid II spritesheets

--- src/org/nhandy/GameConstants.java
+++ src/org/nhandy/GameConstants.java
@@ -1,6 +1,13 @@
 package org.nhandy;
 
 public class GameConstants {
+    /* TODO: WORLD_WIDTH/HEIGHT = SCREEN_WIDTH/HEIGHT
+             Pre-Built Map image resolution = 1152x496 pixels
+             4 different map images
+             vaus resolution = 16x8
+             powerups resolution = 16x8 with an optional shadow of 18x9
+             enemy resolution = 16x16
+    * */
     public static final int WORLD_WIDTH = 2016;
     public static final int WORLD_HEIGHT = 2016;
     public static final int SCREEN_WIDTH = 1300;
